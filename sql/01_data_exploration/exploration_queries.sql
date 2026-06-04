684d0f1d8249463155626a59164b67e13ae25fb5
Sneha Arora <arorasneha11@gmail.com>
2026-06-04 19:13:57 +0100

    Update exploration_queries.sql

--- sql/01_data_exploration/exploration_queries.sql
+++ sql/01_data_exploration/exploration_queries.sql
@@ -1,1 +1,13 @@
+--View sample data
+select *
+from `marketing_analytics.campaign_data`
+limit 10;
 
+--Count total rows
+select count(*) as total_rows 
+from `marketing_analytics.campaign_data`;
+
+--Compare rows vs unique customers, validate CustomerID Uniqueness
+select count(*) as total_rows,
+count(distinct(customerID)) as unique_customers
+from `marketing_analytics.campaign_data`;
